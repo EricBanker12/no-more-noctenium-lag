452eb1e7be99c904513ad1d470f892ecd10e7dfa
Mister-Kay <ericbanker.12@gmail.com>
2017-07-23 11:03:51 -0500

    Skill-Prediction Compatibility Fix
    Skill-Prediction compatibility fix
    Added extra code comments

--- index.js
+++ index.js
@@ -6,51 +6,53 @@ module.exports = function noMoreNocteniumLag(dispatch) {
 	cid = null,
 	enabled = false
 	
-	// S_LOGIN
+	// Get character ID on login and disable noctenium
 	dispatch.hook('S_LOGIN', 2, event => {
 		cid = event.cid
 		enabled = false
 	})
 	
-	// S_ABNORMALITY_BEGIN
+	// Detect noctenium activation
 	dispatch.hook('S_ABNORMALITY_BEGIN', 2, {order: -100}, event => {
+		// if target is your character and noctenium is toggled on, enable
 		if (event.target.equals(cid) && [902, 910, 911, 912, 913, 916].includes(event.id)) {
 			enabled = true
 		}
 	})
 	
-	// S_ABNORMALITY_END
+	// Detect noctenium end
 	dispatch.hook('S_ABNORMALITY_END', 1, {order: -100}, event => {
+		// if target is your character and noctenium is toggled off, disable
 		if (event.target.equals(cid) && [902, 910, 911, 912, 913, 916].includes(event.id)) {
 			enabled = false
 		}
 	})
 	
-	// S_ACTION_STAGE
+	// Get time from starting a skill
 	dispatch.hook('S_ACTION_STAGE', 1, {order: -100}, event => {
 		if (enabled) {
 			timeout = Date.now()
 		}
 	})
 	
-	// S_INVEN
-	dispatch.hook('S_INVEN', 'raw', {order: -100}, code => {
+	// Block S_INVEN if noctenium was used within 100 ms
+	dispatch.hook('S_INVEN', 'raw', {order: 999}, code => {
 		let time_difference = Date.now() - timeout
 		if (time_difference < 100) {
 			return false
 		}
 	})
 	
-	// S_INVEN_CHANGEDSLOT
-	dispatch.hook('S_INVEN_CHANGEDSLOT', 'raw', {order: -100}, code => {
+	// Block S_INVEN_CHANGEDSLOT if noctenium was used within 100 ms
+	dispatch.hook('S_INVEN_CHANGEDSLOT', 'raw', {order: 999}, code => {
 		let time_difference = Date.now() - timeout
 		if (time_difference < 100) {
 			return false
 		}
 	})
 	
-	// S_UPDATE_ACHIEVEMENT_PROGRESS
-	dispatch.hook('S_UPDATE_ACHIEVEMENT_PROGRESS', 'raw', {order: -100}, code => {
+	// Block S_UPDATE_ACHIEVEMENT_PROGRESS if noctenium was used within 1500 ms
+	dispatch.hook('S_UPDATE_ACHIEVEMENT_PROGRESS', 'raw', {order: 999}, code => {
 		let time_difference = Date.now() - timeout
 		if (time_difference < 1500) {
 			return false
