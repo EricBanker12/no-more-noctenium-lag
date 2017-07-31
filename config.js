b8dc04b1551a9c83484d7e6f41653d4210f09248
Mister-Kay <28190493+Mister-Kay@users.noreply.github.com>
2017-07-31 09:05:04 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,12 @@ module.exports = {
 									// always blocks inventory packets
 									// during combat with noctenium except when openning
 									// your inventory.
-									
+									// "None" does not block inventory packets
+	
 	achievementMethod: 'Skill'		// Choose a method for preventing achievement packet lag.
 									// "Skill" method blocks the next achievement packet
 									// after using a skill with noctenium.
 									// "Smart" method made by Pinkie Pie. I can't figure out
 									// how it works. >_<
+									// "None" does not block achievement packets
 }
