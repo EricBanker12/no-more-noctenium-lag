ef6bb6d4a2ccd0a41c04cd661ca1f19c068fda59
Mister-Kay <28190493+Mister-Kay@users.noreply.github.com>
2017-11-26 08:25:08 -0600

    Reorganize for dispatch loading errors
    Reorganized code for all dispatch hooks to be on top most level, so tera-proxy displays proper error for missing protocol mappings.

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ module.exports = function noMoreNocteniumLag(dispatch) {
     debug = false
     
     // Variables
-    let cid = null,            // character ID
-    noctActive = false,        // noctenium status
-    inCombat = false,        // combat status
-    achieves = null,        // for Pinkie Pie's "Smart" achievement method
-    counter = {},            // for skill methods
-    timeout = 0                // Handler variable for failsafe timeout
+    let cid = null,			// character ID
+    noctActive = false,		// noctenium status
+    inCombat = false,		// combat status
+    achieves = null,		// for Pinkie Pie's "Smart" achievement method
+    counter = {},			// for skill methods
+    timeout = 0				// Handler variable for failsafe timeout
     
     // Get character ID on login and disable noctenium
     dispatch.hook('S_LOGIN', 2, {order: -100}, event => {
@@ -49,6 +49,22 @@ module.exports = function noMoreNocteniumLag(dispatch) {
         }
     })
     
+    // Detect combat status
+    dispatch.hook('S_USER_STATUS', 1, {order: -100}, event => {
+        // if character is your character
+        if(event.target.equals(cid)) {
+            // if in combat, set true
+            if (event.status == 1) {
+                inCombat = true
+                if (debug) {console.log('inCombat', inCombat)}
+            }
+            else {
+                inCombat = false
+                if (debug) {console.log('inCombat', inCombat)}
+            }
+        }
+    })
+    
     // Allow after C_SHOW_INVEN
     dispatch.hook('C_SHOW_INVEN', 'raw', {order: -100}, code => {
         // if noctenium active
@@ -60,98 +76,73 @@ module.exports = function noMoreNocteniumLag(dispatch) {
         }
     })
     
-    // Skill method inventory
-    if (config.inventoryMethod.toLowerCase() == 'skill') {
-        // Block S_INVEN
-        dispatch.hook('S_INVEN', 'raw', {order: 999}, code => {
+    // Block S_INVEN
+    dispatch.hook('S_INVEN', 'raw', {order: 999}, code => {
+        // Skill method inventory
+        if (config.inventoryMethod.toLowerCase() == 'skill') {
             let timeDifference = Date.now() - timeout
             if (noctActive && counter.S_INVEN > 0 && timeDifference < 10000) {
                 if (debug) {console.log('S_INVEN Blocked')}
                 counter.S_INVEN -= 1
                 return false
             }
-        })
-        // Block S_INVEN_CHANGEDSLOT
-        dispatch.hook('S_INVEN_CHANGEDSLOT', 'raw', {order: 999}, code => {
-            let timeDifference = Date.now() - timeout
-            if (noctActive && counter.S_INVEN_CHANGEDSLOT > 0 && timeDifference < 10000) {
-                if (debug) {console.log('S_INVEN_CHANGEDSLOT Blocked')}
-                counter.S_INVEN_CHANGEDSLOT -= 1
-                return false
-            }
-        })
-    }
-    
-    // Combat method inventory
-    if (config.inventoryMethod.toLowerCase() == 'combat') {
-        // Detect combat status
-        dispatch.hook('S_USER_STATUS', 1, {order: -100}, event => {
-            // if character is your character
-            if(event.target.equals(cid)) {
-                // if in combat, set true
-                if (event.status == 1) {
-                    inCombat = true
-                    if (debug) {console.log('inCombat', inCombat)}
-                }
-                else {
-                    inCombat = false
-                    if (debug) {console.log('inCombat', inCombat)}
-                }
-            }
-        })
-        // Block S_INVEN
-        dispatch.hook('S_INVEN', 'raw', {order: 999}, code => {
+        }
+        // Combat method inventory
+        if (config.inventoryMethod.toLowerCase() == 'combat') {
             let timeDifference = Date.now() - timeout
             if (noctActive && counter.S_INVEN > 0 && timeDifference < config.timeout && inCombat) {
                 if (debug) {console.log('S_INVEN Blocked')}
                 counter.S_INVEN -= 1
                 return false
             }
-        })
-        // Block S_INVEN_CHANGEDSLOT
-        dispatch.hook('S_INVEN_CHANGEDSLOT', 'raw', {order: 999}, code => {
+        }
+    })
+    
+    // Block S_INVEN_CHANGEDSLOT
+    dispatch.hook('S_INVEN_CHANGEDSLOT', 'raw', {order: 999}, code => {
+        // Skill method inventory
+        if (config.inventoryMethod.toLowerCase() == 'skill') {
+            let timeDifference = Date.now() - timeout
+            if (noctActive && counter.S_INVEN_CHANGEDSLOT > 0 && timeDifference < 10000) {
+                if (debug) {console.log('S_INVEN_CHANGEDSLOT Blocked')}
+                counter.S_INVEN_CHANGEDSLOT -= 1
+                return false
+            }
+        }
+        // Combat method inventory
+        if (config.inventoryMethod.toLowerCase() == 'combat') {
             let timeDifference = Date.now() - timeout
             if (noctActive && counter.S_INVEN_CHANGEDSLOT > 0 && timeDifference < config.timeout && inCombat) {
                 if (debug) {console.log('S_INVEN_CHANGEDSLOT Blocked')}
                 counter.S_INVEN_CHANGEDSLOT -= 1
                 return false
             }
-        })
-    }
+        }
+    })
     
-    // Skill method achievements
-    if (config.achievementMethod.toLowerCase() == 'skill') {
-        dispatch.hook('S_UPDATE_ACHIEVEMENT_PROGRESS', 'raw', {order: 999}, code => {
+    // Achievements
+    dispatch.hook('S_UPDATE_ACHIEVEMENT_PROGRESS', 'raw', {order: 999}, code => {
+        // Skill method achievements
+        if (config.achievementMethod.toLowerCase() == 'skill') {
             let timeDifference = Date.now() - timeout
             if (noctActive && counter.S_UPDATE_ACHIEVEMENT_PROGRESS > 0 && timeDifference < config.timeout) {
                 if (debug) {console.log('S_UPDATE_ACHIEVEMENT_PROGRESS Blocked')}
                 counter.S_UPDATE_ACHIEVEMENT_PROGRESS -= 1
                 return false
             }
-        })
-    }
-    
-    // Combat method achievements
-    if (config.achievementMethod.toLowerCase() == 'combat') {
-        dispatch.hook('S_UPDATE_ACHIEVEMENT_PROGRESS', 'raw', {order: 999}, code => {
+        }
+        // Combat method achievements
+        if (config.achievementMethod.toLowerCase() == 'combat') {
             let timeDifference = Date.now() - timeout
             if (noctActive && counter.S_UPDATE_ACHIEVEMENT_PROGRESS > 0 && timeDifference < config.timeout && inCombat) {
                 if (debug) {console.log('S_UPDATE_ACHIEVEMENT_PROGRESS Blocked')}
                 counter.S_UPDATE_ACHIEVEMENT_PROGRESS -= 1
                 return false
             }
-        })
-    }
-    
-    // Method created by Pinkie Pie (https://github.com/pinkipi)
-    if (config.achievementMethod.toLowerCase() == 'smart') {
-        // S_LOAD_ACHIEVEMENT_LIST
-        dispatch.hook('S_LOAD_ACHIEVEMENT_LIST', 1, event => {
-            achieves = {}
-            if (debug) {console.log('S_LOAD_ACHIEVEMENT_LIST')}
-        })
-        // S_UPDATE_ACHIEVEMENT_PROGRESS
-        dispatch.hook('S_UPDATE_ACHIEVEMENT_PROGRESS', 1, event => {
+        }
+        // Smart method achievements
+        // Method created by Pinkie Pie (https://github.com/pinkipi)
+        if (config.achievementMethod.toLowerCase() == 'smart') {
             if (debug) {console.log('S_UPDATE_ACHIEVEMENT_PROGRESS')}
             let achievements = []
 
@@ -163,11 +154,20 @@ module.exports = function noMoreNocteniumLag(dispatch) {
             }
             Object.assign(event, {achievements})
             return true
-        })
-        // comparison function
-        function compareJSON(obj1, obj2) {
-            if (debug) {console.log('compareJSON')}
-            return JSON.stringify(obj1) == JSON.stringify(obj2)
         }
+    })
+    
+    // comparison function
+    function compareJSON(obj1, obj2) {
+        if (debug) {console.log('compareJSON')}
+        return JSON.stringify(obj1) == JSON.stringify(obj2)
     }
+    
+    // S_LOAD_ACHIEVEMENT_LIST
+    dispatch.hook('S_LOAD_ACHIEVEMENT_LIST', 1, event => {
+        if (config.achievementMethod.toLowerCase() == 'smart') {
+            achieves = {}
+            if (debug) {console.log('S_LOAD_ACHIEVEMENT_LIST')}
+        }
+    })
 }
